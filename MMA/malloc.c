4c2872ef2406753d610929be20bd6f867b8217e6
FebArch <febarchapray@gmail.com>
2026-06-27 00:46:43 +0530

    malloc

--- MMA/malloc.c
+++ MMA/malloc.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void displayArray(int *arr, int len);
 void displayArrElementAddresses(int *arr, int len);
 
+int* mulTable(int m);
+
 int main(){
     int arr[4] = {12,5,67,8};
     int* h_arr = malloc(4*sizeof(int));
-    displayArray(arr, 4);
-    displayArrElementAddresses(arr, 4);
+    // displayArray(arr, 4);
+    
+    // displayArrElementAddresses(arr, 4);
+    // displayArrElementAddresses(h_arr, 4);
 
+    int *h_table = mulTable(2);
+    displayArray(h_table, 10);
 
     return 0;
 }
@@ -29,4 +36,15 @@ void displayArrElementAddresses(int* arr, int len){
         printf("%p, ", (arr+i));
     }
     printf("\b\b}\n");
+}
+
+int* mulTable(int m){
+    int* table = malloc(10*sizeof(int));
+
+    for (int n = 0; n < 10; n++)
+    {
+        *(table+n) = m * (n+1);
+        // printf("%d    *%p\n", *(table+n), (table+n));
+    }
+    return table;
 }
