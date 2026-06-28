468285ed68b4e1411b99f4fc8fc8c545e8d06f70
FebArch <febarchapray@gmail.com>
2026-06-28 10:41:13 +0530

    calloc done

--- MMA/calloc.c
+++ MMA/calloc.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void* heapArrayDeclaration(int bytes);
+void* heapArrayDeclaration(int size, int bytes);
 void heapArrayInitialization(float *ptr, int len);
 void displayHeapArray(float* arr, int len);
+void displayHeapArrayAddresses(float* arr, int len);
 
 int main(){
     int n=5;
-    float *h_arr = heapArrayDeclaration(n*sizeof(float));
-    heapArrayInitialization(h_arr, n);
+    float *h_arr = heapArrayDeclaration(sizeof(float), n);
+    // heapArrayInitialization(h_arr, n);
     displayHeapArray(h_arr, n);
+    // displayHeapArrayAddresses(h_arr, n);
     return 0;
 }
 
-void* heapArrayDeclaration(int bytes){
-    return malloc(bytes);
+void* heapArrayDeclaration(int size, int bytes){
+    return calloc(size, bytes);
 }
 
 
@@ -37,7 +39,17 @@ void displayHeapArray(float* arr, int len){
     printf("{");
     for (int i = 0; i < len; i++)
     {
-        printf("%.4f, ", *(arr+i));
+        printf("%d, ", *(arr+i));
+    }
+    printf("\b\b}\n");
+}
+
+
+void displayHeapArrayAddresses(float* arr, int len){
+    printf("{");
+    for (int i = 0; i < len; i++)
+    {
+        printf("%p, ", (arr+i));
     }
     printf("\b\b}\n");
 }
