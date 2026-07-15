0ee0aaead96241fac097103f8b0bd7b8314dbce8
FebArch <febarchapray@gmail.com>
2026-07-15 11:44:47 +0530

    locality check with time lib

--- book/threeDArrayLocality.c
+++ book/threeDArrayLocality.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(){
+    clock_t start, end;
+    double cpu_time;
+
+    start = clock();
     int arr[3][3][3] = {
         { {1,2,3}, {4,5,6}, {7,8,9} },
         { {11,12,13}, {14,15,16}, {17,18,19} },
@@ -13,11 +18,18 @@ int main(){
         for (int j = N-1; j >= 0; j--)
         {
             for (int k = N-1; k >= 0; k--){
-                product *= arr[j][k][i];   //  Poor Spatial Locality
+                // product *= arr[j][k][i];   //  Poor Spatial Locality
                 product *= arr[i][j][k];   //  Good Spatial Locality
             }
         }
     }
     printf("Product of all elements in 3d Array is: %d\n", product);
+    end = clock();
+
+    cpu_time = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Test for Poor Spatial Locality arr[j][k][i]\n");
+    printf("CPU Time usage: %f seconds", cpu_time);
+
+    return 0;
 
 }
